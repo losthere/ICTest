87235e54887cd4e4cc06bdda64fa812479493a1d
Swati Rathaur <swatirathaur@microsoft.com>
2023-02-25 14:32:54 +0530

    first commit

---
+++ XamlIsland/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using IC;
+
+namespace XamlIsland
+{
+    public partial class App : System.Windows.Application
+    {
+        protected override void OnStartup(System.Windows.StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var app = new XamlIsland.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
---
+++ XamlIsland/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using Windows.UI.Xaml.Controls;
+
+namespace XamlIsland
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
